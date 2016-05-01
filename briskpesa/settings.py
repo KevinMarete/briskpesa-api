769f7c8c27d731f160698e5083e93783f950437c
Rodgers Andati <andatirodgers@gmail.com>
2016-05-01 11:26:55 +0300

    changed static root

--- briskpesa/settings.py
+++ briskpesa/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ur-l2(4wkv=)&_=&j#ahq7u+&s(h)&jsgnhixle5ha6uen6y&q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'briskpesapp.User'
